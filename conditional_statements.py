6fcf3a0a046540bca2fe3c862a62d366c52666a9
awashValley <hammerson1@gmail.com>
2016-01-28 12:30:27 +0100

    Create conditional_statements.py

---
+++ conditional_statements.py
@@ -0,0 +1,10 @@
+
+# Example if/elif/else statement
+  temperature = int(input("What is the temperature in Fahrenheit? "))
+  if temperature > 90:
+    print("It is hot outside")
+  elif temperature < 30:
+    print("It is cold outside")
+  else:
+    print("It is not hot outside")
+  print("Done")
